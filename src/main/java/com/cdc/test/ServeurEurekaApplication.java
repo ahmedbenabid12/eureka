f1ae8c5f1d271f6e92aea5036e74bac9ddea292c
Ahmed BEN ABID <ahmed.benabid12@gmail.com>
2021-11-08 13:09:32 +0100

    V1

---
+++ src/main/java/com/cdc/test/ServeurEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.cdc.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer //pour que le serveur démarre automatiquement 
+public class ServeurEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurEurekaApplication.class, args);
+	}
+// modification a traves eclipse
+}
---
+++ src/test/java/com/cdc/test/ServeurEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdc.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
